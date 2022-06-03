2de152f49af68ebd8b56749dc39eca91034a6157
YaMiN <yamin8000@yahoo.com>
2022-06-04 00:03:18 +0430

    splash screen bug fix

--- app/src/main/java/io/github/yamin8000/cafe/base/SplashFragment.kt
+++ app/src/main/java/io/github/yamin8000/cafe/base/SplashFragment.kt
@@ -15,6 +15,7 @@ import io.github.yamin8000.cafe.util.Utility.handleCrash
 class SplashFragment : BaseFragment<FragmentSplashBinding>({ FragmentSplashBinding.inflate(it) }) {
 
     private lateinit var handler: Handler
+    private var isSplashShown = false
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -22,8 +23,7 @@ class SplashFragment : BaseFragment<FragmentSplashBinding>({ FragmentSplashBindi
         try {
             handler = Handler(Looper.getMainLooper())
             handler.postDelayed(SPLASH_DELAY) {
-                if (!this.isDetached)
-                    findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+                navigateToSearchFragment()
             }
         } catch (exception: Exception) {
             handleCrash(exception)
@@ -32,7 +32,21 @@ class SplashFragment : BaseFragment<FragmentSplashBinding>({ FragmentSplashBindi
 
     override fun onPause() {
         super.onPause()
+
+        isSplashShown = true
         if (this::handler.isInitialized)
             handler.removeCallbacksAndMessages(null)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (isSplashShown)
+            navigateToSearchFragment()
+    }
+
+    private fun navigateToSearchFragment() {
+        if (!this.isDetached) {
+            findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+        }
     }
 }

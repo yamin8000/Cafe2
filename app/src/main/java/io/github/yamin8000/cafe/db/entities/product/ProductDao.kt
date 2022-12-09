b833f12bafbb4b426a32101bf84086372fe3e21b
YaMiN <yamin8000@yahoo.com>
2022-12-09 11:51:51 +0330

    copyright added

--- app/src/main/java/io/github/yamin8000/cafe/db/entities/product/ProductDao.kt
+++ app/src/main/java/io/github/yamin8000/cafe/db/entities/product/ProductDao.kt
@@ -1,3 +1,24 @@
+/*
+ *     Cafe/Cafe.app.main
+ *     ProductDao.kt Copyrighted by Yamin Siahmargooei at 2022/12/9
+ *     ProductDao.kt Last modified at 2022/12/9
+ *     This file is part of Cafe/Cafe.app.main.
+ *     Copyright (C) 2022  Yamin Siahmargooei
+ *
+ *     Cafe/Cafe.app.main is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Cafe/Cafe.app.main is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Cafe.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package io.github.yamin8000.cafe.db.entities.product
 
 import androidx.room.Dao
